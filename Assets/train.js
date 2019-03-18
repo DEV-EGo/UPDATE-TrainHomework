9e2ac360ee9f264536bef5fd29eaadbde864e5ff
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2019-03-17 22:07:49 -0400

    update

--- Assets/train.js
+++ Assets/train.js
@@ -37,28 +37,27 @@ $("#submit").on("click", function (event) {
         dateAdded: firebase.database.ServerValue.TIMESTAMP
 
     });
-});
 
-database.ref().on("child_added", function (childSnapshot) {
+    database.ref().on("child_added", function (childSnapshot) {
 
-    var Frequency = $("#Frequency-Input").val();
-    var Time = $("Time-input").val();
+        var Frequency = $("#Frequency-Input").val();
+        var Time = $("Time-input").val();
 
-    var ConvertedTime = moment(NewTime, "HH:mm").subtract(1, "years");
-    var CurrenTime = moment();
+        var ConvertedTime = moment(NewTime, "HH:mm").subtract(1, "years");
+        var CurrenTime = moment();
 
-    var DifferentTime = moment().diff(moment(timeConverted), "minutes");
-    var TimeRemainding = DifferentTime % Frequency;
-    var minutes = Frequency - TimeRemainding;
-    var Arrival = moment().add(minutes, "minutes");
+        var DifferentTime = moment().diff(moment(timeConverted), "minutes");
+        var TimeRemainding = DifferentTime % Frequency;
+        var minutes = Frequency - TimeRemainding;
+        var Arrival = moment().add(minutes, "minutes");
 
-    $("TrainSchedule").append({
-        childSnapshot.val().train;
-        childSnapshot.val().destination;
-        childSnapshot.val().frequency + moment(arrival).format("hh:mm");
-    }),
+        $("TrainSchedule").append({
+            childSnapshot.val().train;
+            childSnapshot.val().destination;
+            childSnapshot.val().frequency + moment(arrival).format("hh:mm");
+        }),
 
-        function (errorObject) {
-            console.log("Errors handled: " + errorObject.code);
+            function (errorObject) {
+                console.log("Errors handled: " + errorObject.code);
 
-        });
+            };
